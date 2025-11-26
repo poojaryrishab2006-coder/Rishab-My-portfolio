cbdd369cc09b376b6dfa04754d71e1a784665f63
Rishab poojary <poojaryrishab.2006@gmail.com>
2025-11-26 18:31:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ ScrollReveal({
 
 
 const typed = new Typed('.multiple-text',{
-    strings:['Frontend Developer!!','Editor','DJ'],
+    strings:['BCA Student!!','Editor','DJ'],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
